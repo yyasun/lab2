d3fe9c7a36032928ada9eab8b072757114dba43a
Yaroslav <pablo1231435@gmail.com>
2020-06-03 11:38:22 +0300

    added input

--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@ namespace Lab2
 {
     class Program
     {
+        static string ReadInput()
+        {
+            Console.WriteLine("Enter directory name:");
+            return Console.ReadLine();
+        }       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
