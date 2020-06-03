a5c7d6a3d115f881ea3589d56a3df5098fd91006
Yaroslav <pablo1231435@gmail.com>
2020-06-03 11:46:17 +0300

    added student model and studentparser class

---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab2
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public IEnumerable<int> Grades { get; set; }
+        public bool IsContractor { get; set; }
+
+    }
+}
---
+++ StudentParser.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab2
+{
+    public class StudentParser
+    {
+        
+    }
+}
